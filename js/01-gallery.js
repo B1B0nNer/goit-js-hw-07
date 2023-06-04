376eabd7bc8b323d4a4a1196250c0cec20c44139
Nikita <barbos.nik007@gmail.com>
2023-06-05 01:26:44 +0200

    firstTask

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const photos = galleryItems.map((photo) => {
+  const image = document.createElement('img');
+  image.src = photo.preview;
+  image.alt = photo.description;
+  image.dataset.source = photo.original;
+  image.classList.add('gallery__item');
+
+  image.addEventListener('click', handlePhotoClick);
+
+  return image;
+});
+
+gallery.append(...photos);
+
+const lightbox = basicLightbox.create(`
+  <div class="modal">
+    <img src="" alt="" class="modal__image">
+  </div>
+`);
+
+function handlePhotoClick(event) {
+  const clickedPhoto = event.target;
+
+  lightbox.element().querySelector('.modal__image').src = clickedPhoto.dataset.source;
+  lightbox.show();
+}
