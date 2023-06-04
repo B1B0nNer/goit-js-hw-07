c9c35dd461f1d81e6ef81be2ab23f4f81a586c5f
Nikita <barbos.nik007@gmail.com>
2023-06-05 01:44:33 +0200

    Second one

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const photos = galleryItems.map((photo) => {
+  const image = document.createElement('img');
+  image.src = photo.preview;
+  image.alt = photo.description;
+  image.setAttribute('data-source', photo.original);
+  image.classList.add('gallery__item');
+
+  return image;
+});
+
+gallery.append(...photos);
+
+const lightbox = new Lightbox('.gallery__item');
