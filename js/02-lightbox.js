f6c13366a5fe77a9f5a63fa2b2fe41c1b14b4e23
Nikita <barbos.nik007@gmail.com>
2023-06-09 16:29:35 +0200

    Done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,33 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
-const photos = galleryItems.map((photo) => {
+const photos = galleryItems.map(({preview, original, description}) => {
 
-  const li = document.createElement("li");
-  li.classList.add("gallery__item");
-
-  const a = document.createElement("a");
-  a.href = photo.original;
-  a.dataLightbox = "lbox";
-  a.classList.add("gallery__link");
-
-  const image = document.createElement('img');
-  image.src = photo.preview;
-  image.alt = photo.description;
-  image.dataset.source = photo.original;
-  image.classList.add('gallery__item');
-  image.classList.add("gallery__image")
-
-  a.append(image);
-  li.append(a);
-
-  return li;
+  const lishka = 
+  `<li class="gallery__item">
+      <a class="galley__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+      </a>
+    </li>`
+    return lishka;
 });
 
-gallery.append(...photos);
+gallery.insertAdjacentHTML('beforeend', photos.join(''));
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captions: true,
-  captionData: 'alt',
+  captionsData: 'alt',
   captionDelay: 250,
 });
